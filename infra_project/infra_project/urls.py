3daec2bdffce117058ce348b7ec66d43468ebdb5
Alexander Gryaznov <ne4istii@gmail.com>
2021-05-18 12:14:48 +0300

    fix flake8 errors

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,5 +1,6 @@
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path, include
+
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
